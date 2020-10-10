977df44fd45704d8af7f81c5278f2ac48a5f230a
Alexander Misel <7542439+AlexanderMisel@users.noreply.github.com>
2020-10-10 11:24:40 +0800

    Update latex2unicode.lua

--- latex2unicode.lua
+++ latex2unicode.lua
@@ -13,13 +13,14 @@ local latex_lib = {
       return a .. '/' .. b
     end
   },
-  mathrm = {
+  textrm = {
     arg_num = 1,
     func = function(a) return a end
   }
 }
 
 latex_lib.dfrac = latex_lib.frac
+latex_lib.mathrm = latex_lib.textrm
 
 do
   local status, err = pcall(function()
@@ -35,28 +36,34 @@ do
   end
 end
 
-local trans_data = {}
 local trans_pat = {
   subscripts = re.compile("([0-9()aeh-pr-vx+=-] /'β'/'γ'/'ρ'/'φ'/'χ')+ !."),
   superscripts = re.compile("([0-9()a-zABD-PRT-W+=-] /'α'/'β'/'γ'/'δ'/'∊'/'θ'/'ι'/'Φ'/'φ'/'χ')+ !.")
 }
 
-function load_data(name)
+function load_data(name, alias)
   local f, err = io.open('data/' .. name)
   if not f then
     return print(err)
   end
-  trans_data[name] = {}
+  latex_lib[name] = {}
   for line in f:lines() do
     local cmd, char = line:match('^(%S+) (.+)$')
     if cmd and char then
-      trans_data[name][cmd] = char
+      latex_lib[name][cmd] = char
     end
   end
+  if alias then latex_lib[alias] = latex_lib[name] end
 end
 
 load_data('subscripts')
 load_data('superscripts')
+load_data('textbb', 'mathbb')
+load_data('textbf', 'mathbf')
+load_data('textcal', 'mathcal')
+load_data('textfrak', 'mathfrak')
+load_data('textit', 'mathit')
+load_data('textmono', 'mathtt')
 
 local defs = {
   merge = function(a, b)
@@ -72,6 +79,10 @@ local defs = {
       if type(a) == 'string' then
         if b then return a .. b .. (c or '')
         else return a end
+      elseif type(a) == 'table' then
+        return b:gsub('[%z\1-\127\194-\244][\128-\191]*', function(p)
+          return a[p]
+        end) .. (c or '')
       elseif a.arg_num then
         if a.arg_num == 1 then
           return a.func(b) .. (c or '')
@@ -85,35 +96,31 @@ local defs = {
     elseif b then return a .. ' ' .. b .. (c and (' ' .. c) or '')
     else return a end
   end,
-  transform = function(a, b)
-    --print('transform', a, b)
-    if a == 'subscripts' or a == 'superscripts' then
-      if trans_pat[a]:match(b) then
-        return b:gsub('[%z\1-\127\194-\244][\128-\191]*', function(p)
-          return trans_data[a][p]
-        end)
-      else
-        local stext = (b:ulen() == 1 and b or ('{' .. b .. '}'))
-        if a == 'subscripts' then return '_' .. stext
-        else return '^' .. stext end
-      end
-    else
+  subsup = function(a, b)
+    local dict = latex_lib[a]
+    if dict:match(b) then
       return b:gsub('[%z\1-\127\194-\244][\128-\191]*', function(p)
-        return trans_data[a][b]
+        return dict[p]
       end)
+    else
+      local stext = (b:ulen() == 1 and b or ('{' .. b .. '}'))
+      if a == 'subscripts' then return '_' .. stext
+      else return '^' .. stext end
     end
   end
 }
 
 local grammar = re.compile([=[--lpeg
-  equation  <- block+ ~> merge
-  block     <- space / macro / subscript / superscript / parameter / {[^{}]}
-  space     <- '\' ([,:;! ] / 'q'? 'quad') -> ' '
-  bare_macro <- ('\' {%a+} ) -> gen_macro
-  macro     <- ('\' {%a+} (parameter parameter?)? ) -> gen_macro
-  subscript <- ('_' ''->'subscripts' parameter )  -> transform
-  superscript  <- ('^' ''->'superscripts' parameter )  -> transform
-  parameter <- '{' equation '}' / bare_macro / { [^%s^_{}] }
+  equation    <- block+ ~> merge
+  block       <- space / macro / escape / subscript / superscript / parameter / {[^{}]}
+  space       <- '\' ([,:;! ] / 'q' 'q'? 'uad') -> ' '
+  bare_macro  <- ('\' {%a+} ) -> gen_macro
+  macro       <- ('\' {%a+} (parameter parameter?)? ) -> gen_macro
+  escape      <- '\' {.}
+  
+  subscript   <- ('_' ''->'subscripts' parameter )  -> subsup
+  superscript <- ('^' ''->'superscripts' parameter )  -> subsup
+  parameter   <- '{' equation '}' / bare_macro / { [^%s^\_{}] }
 ]=], defs)
 
-print(grammar:match([[B' =-\nabla \times E]]))
+print(grammar:match([[\int y \mathrm{d}x]]))
