fe74fdcea831cee8bbe0779b87f25a8de2d520c4
Alexander Misel <7542439+AlexanderMisel@users.noreply.github.com>
2020-10-11 22:52:09 +0800

    Add files via upload

--- latex2unicode.lua
+++ latex2unicode.lua
@@ -1,134 +1,136 @@
 local re = require('lpeg.re')
-
-function string:ulen()
-  return select(2, self:gsub('[^\128-\193]', ''))
-end
-
-local latex_lib = {
-  frac = {
-    arg_num = 2,
-    func = function(a, b)
-      local a = a:ulen() == 1 and a or ('(' .. a .. ')')
-      local b = b:ulen() == 1 and b or ('(' .. b .. ')')
-      return a .. '/' .. b
-    end
-  },
-  echo = {
-    arg_num = 1,
-    func = function(a) return a end
-  }
-}
-
-latex_lib.dfrac = latex_lib.frac
-latex_lib.textrm = latex_lib.echo
-latex_lib.left = latex_lib.echo
-latex_lib.right = latex_lib.echo
-latex_lib.mathrm = latex_lib.textrm
-
-do
-  local status, err = pcall(function()
-    for line in io.lines('data/symbols') do
-      local cmd, char = line:match('^\\(%w+) (.+)$')
-      if cmd and char then
-        latex_lib[cmd] = char
-      end
-    end
-  end)
-  if not status then
-    print(err)
-  end
-end
+local latex_lib = require('latex_lib')
 
 local trans_pat = {
   subscripts = re.compile("([0-9()aeh-pr-vx+=-] /'β'/'γ'/'ρ'/'φ'/'χ')+ !."),
   superscripts = re.compile("([0-9()a-zABD-PRT-W+=-] /'α'/'β'/'γ'/'δ'/'∊'/'θ'/'ι'/'Φ'/'φ'/'χ')+ !.")
 }
 
-function load_data(name, alias)
-  local f, err = io.open('data/' .. name)
-  if not f then
-    return print(err)
-  end
-  latex_lib[name] = {}
-  for line in f:lines() do
-    local cmd, char = line:match('^(%S+) (.+)$')
-    if cmd and char then
-      latex_lib[name][cmd] = char
-    end
-  end
-  if alias then latex_lib[alias] = latex_lib[name] end
-end
-
-load_data('subscripts')
-load_data('superscripts')
-load_data('textbb', 'mathbb')
-load_data('textbf', 'mathbf')
-load_data('textcal', 'mathcal')
-load_data('textfrak', 'mathfrak')
-load_data('textit', 'mathit')
-load_data('textmono', 'mathtt')
-
 local defs = {
-  merge = function(a, b)
-    --print('merge', a, b)
-    return a .. b
-  end,
-  gen_macro = function(a, b, c)
-    --print('gen_macro', a, b, c)
-    local func_name = a
-    if latex_lib[a] then
-      a = latex_lib[a]
-      --print(a)
-      if type(a) == 'string' then
-        if b then return a .. b .. (c or '')
-        else return a end
-      elseif type(a) == 'table' then
-        if not a.arg_num then
-          return b:gsub('[%z\1-\127\194-\244][\128-\191]*', function(p)
-            return a[p]
-          end) .. (c or '')
-        else
-          if a.arg_num == 1 then
-            return a.func(b) .. (c or '')
-          elseif a.arg_num == 2 then
-            if not c then error(func_name .. ' second parameter cannot be nil') end
-            return a.func(b, c)
-          else
-            error('unsupported number of arguments')
-          end
-        end
-      else error('unsupported latex lib type: ' .. type(a))
-      end
-    elseif b then return a .. ' ' .. b .. (c and (' ' .. c) or '')
-    else return a end
-  end,
-  subsup = function(a, b)
-    --print('subsup: ', a, b)
-    local dict = latex_lib[a]
-    if trans_pat[a]:match(b) then
-      return b:gsub('[%z\1-\127\194-\244][\128-\191]*', function(p)
-        return dict[p]
-      end)
-    else
-      local stext = (b:ulen() == 1 and b or ('{' .. b .. '}'))
-      if a == 'subscripts' then return '_' .. stext
-      else return '^' .. stext end
-    end
-  end
+  newline = '\n',
+  eb = ']'
 }
 
 local grammar = re.compile([=[--lpeg
-  equation    <- block+ ~> merge
-  block       <- space / macro / escape / subscript / superscript / parameter / {[^{}]}
+  equation    <- {| (%s* block)+ |}
+  block       <- &'\' (space / macro / newline / escape) / subscript / superscript
+                  / '{' (equation / {''}) '}' / {[^\{}_^[%eb]}
   space       <- '\' ([,:;! ] / 'q' 'q'? 'uad') -> ' '
-  bare_macro  <- ('\' {%a+} ) -> gen_macro
-  macro       <- ('\' {%a+} (%s* parameter (%s* parameter)?)? ) -> gen_macro
+  macro       <- {'\' %a+} ('[' -> 'optional' (equation / {''}) ']')?
+  newline     <- '\\' -> newline
   escape      <- '\' {.}
   
-  subscript   <- ('_' ''->'subscripts' parameter )  -> subsup
-  superscript <- ('^' ''->'superscripts' parameter )  -> subsup
-  parameter   <- '{' equation '}' / bare_macro / { [^%s^\_{}] }
+  subscript   <- {'_'} parameter
+  superscript <- {'^'} parameter
+  parameter   <-  / { [^%s{}] }
 ]=], defs)
 
-print(grammar:match([[\frac{\mathrm d}{\mathrm d x} \left( k g(x) \right)
-]]))
+local function subsup(a, b)
+  --print('subsup: ', a, b)
+  local dict = latex_lib[a]
+  if trans_pat[a]:match(b) then
+    return b:gsub('[%z\1-\127\194-\244][\128-\191]*', function(p)
+      return dict[p]
+    end)
+  else
+    local stext = (b:ulen() == 1 and b or ('{' .. b .. '}'))
+    if a == 'subscripts' then return '_' .. stext
+    else return '^' .. stext end
+  end
+end
+
+local expand_parameter = nil
+
+local function walk(expr)
+  local s = ''
+  local i, v = 1, expr[i]
+  
+  local function next_token(prev_token)
+      i = i + 1
+      local p = expr[i]
+      if not p then
+        error("Expected group for '" .. prev_token .."'")
+      end
+      return p
+  end
+  
+  while true do
+    v = expr[i]
+    if not v then break end
+    if type(v) == 'string' then
+      if v == '_' then
+        local p = next_token('_')
+        s = s .. subsup('subscripts', expand_parameter(p))
+      elseif v == '^' then
+        local p = next_token('^')
+        s = s .. subsup('superscripts', expand_parameter(p))
+      elseif v:sub(1, 1) == '\\' then
+        local func = v:sub(2)
+        local a = latex_lib[func]
+        if a then
+          if type(a) == 'string' then
+            s = s .. a
+          elseif not a.arg_num then
+            local b = next_token(func)
+            s = s .. b:gsub('[%z\1-\127\194-\244][\128-\191]*', function(p)
+              return a[p]
+            end)
+          else
+            local arguments = {}
+            local op_arg = nil
+            local j = 1
+            while j <= a.arg_num do
+              local the_arg = expand_parameter(next_token(func .. '#' .. j))
+              if the_arg == 'optional' then
+                op_arg = expand_parameter(next_token(func))
+                j = j - 1
+              else
+                arguments[j] = the_arg
+              end
+              j = j + 1
+            end
+            if op_arg then
+              table.insert(arguments, op_arg)
+            end
+            local sp = a.space and ' ' or ''
+            s = s .. sp .. a.func(unpack(arguments)) .. sp
+          end
+        else
+          error('Unsupported macro: ' .. func)
+        end
+      else
+        s = s .. v
+      end
+    else -- table
+      s = s .. walk(v)
+    end
+    i = i + 1
+  end
+  return (s:gsub('^ ', ''):gsub(' $', ''))
+end
+
+local inspect = require('inspect')
+
+expand_parameter = function(p)
+  --print('expand_parameter', p)
+  if type(p) == 'string' then
+    if #p > 1 and p:sub(1, 1) == '\\' then
+      local res = latex_lib[p:sub(2)]
+      if type(res) ~= 'string' then
+        error('Expected group instead of macro: ' .. p)
+      end
+      return res
+    else
+      return p
+    end
+  else -- table
+    return walk(p)
+  end
+end
+
+local tree = grammar:match([[
+\frac{2}{4}=0.5
+]])
+print(inspect(tree))
+
+print(walk(tree))
